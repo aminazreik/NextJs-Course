7c408ca5c0fffbd66dcce73001955a634f640efb
Amina Zreik <aminazreik95@outlook.com>
2022-10-11 16:53:48 -0400

    some set up but not working

--- nextjs-project-routing-version/src/App.js
+++ nextjs-project-routing-version/src/App.js
@@ -1,5 +1,20 @@
+import React from "react";
+import { Route, Routes, BrowserRouter } from "react-router-dom";
+
+import AllMeetupsPage from "./pages/AllMeetups";
+import NewMeetupPage from "./pages/NewMeetup";
+import FavoritesPage from "./pages/Favorites";
+
 function App() {
-  return <div></div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<AllMeetupsPage />} />
+        <Route path="/new-meetup" element={<NewMeetupPage />} />
+        <Route path="/favorites" element={<FavoritesPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
